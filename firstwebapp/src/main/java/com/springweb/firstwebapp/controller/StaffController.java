9a1f9fb4f5b767a0cb86fb431f182a7b7319881e
saravanan.mca1992@gmail.com <53892651+saravanan-prog@users.noreply.github.com>
2025-10-08 11:43:36 +0530

    patch mapping and put mapping

--- firstwebapp/src/main/java/com/springweb/firstwebapp/controller/StaffController.java
+++ firstwebapp/src/main/java/com/springweb/firstwebapp/controller/StaffController.java
@@ -1,5 +1,4 @@
 package com.springweb.firstwebapp.controller;
-
 import com.springweb.firstwebapp.model.StaffModel;
 import com.springweb.firstwebapp.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +13,53 @@ public class StaffController {
     private StaffService serviceobj;
 
     @GetMapping("getAllStaffList")
-    public List<StaffModel> listallStaffs(){
-        return this.serviceobj.getStaffList();
+
+    public List<StaffModel> listallStaffs(
+            @RequestHeader("token") String token
+    ){
+        if(token.equals("saravanan-security-api")){
+            System.out.println("True block");
+            return this.serviceobj.getStaffList();
+        }
+        else{
+            return null;
+        }
+
     }
 
-    @PostMapping("/addStaff")
+    @PostMapping("addStaff")
     public String addStaffList(
             @RequestBody StaffModel staffPayload
     ){
         return this.serviceobj.addStaffList(staffPayload);
     }
 
+    @PutMapping("modifyStaff/{id}")
+    public List<StaffModel> modifyStaffDetails(
+            @PathVariable long id,
+            @RequestBody StaffModel updateStaffPayload
+    ){
+        return this.serviceobj.ModifyStaffDetailsStreamApi(id,updateStaffPayload);
+    }
 
 
+    @PatchMapping("staffNameFileldChange/{id}")
+    public String modifyStaffFields(
+            @PathVariable long id,
+            @RequestBody StaffModel actualPayload
+    ){
+
+        return this.serviceobj.modifyField(id,actualPayload);
+    }
+
+
+    @DeleteMapping("/recordDelete/{id}")
+    public String removeStaffDetail(
+            @PathVariable long id
+    ){
+
+
+        return this.serviceobj.removeStaff(id);
+    }
 
 }
--- firstwebapp/src/main/java/com/springweb/firstwebapp/service/StaffService.java
+++ firstwebapp/src/main/java/com/springweb/firstwebapp/service/StaffService.java
@@ -5,14 +5,27 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class StaffService {
 
     List<StaffModel> staff = new ArrayList<StaffModel>();
 
+    public StaffService(){
+        staff = Arrays.asList(
+                new StaffModel(1,"Raja","MCA","IT","chennai",100000),
+                new StaffModel(2,"kamal","B.tech","IT","Bangalore",10000),
+                new StaffModel(3,"Rishab","B.tech","IT","Mumbai",250000)
+        );
+    }
+
     public List<StaffModel> getStaffList(){
+        System.out.println("Service method is invoked");
         return staff;
     }
 
@@ -20,4 +33,66 @@ public class StaffService {
         staff.add(staffPayload);
         return "Staff detail added successfully";
     }
+
+    public String modifyStaffDetails(long id,StaffModel actualPayload){
+
+        for(StaffModel item : staff){
+            if(item.getStaffId() == id){
+                item.setStaffId(actualPayload.getStaffId());
+                item.setStaffName(actualPayload.getStaffName());
+                item.setStaffQualifaction(actualPayload.getStaffQualifaction());
+                item.setStaffDepartment(actualPayload.getStaffDepartment());
+                item.setStaffAddress(actualPayload.getStaffAddress());
+                item.setStaffSalary(actualPayload.getStaffSalary());
+
+                return "Record Modified Successfully";
+            }
+        }
+
+
+
+        return "ID is Not found";
+    }
+    public List<StaffModel> ModifyStaffDetailsStreamApi(long id,StaffModel actualPayload){
+        List<StaffModel> updatedStaff = staff.stream()
+                .filter(item -> item.getStaffId() == id)
+                .peek(
+                        item -> {
+                            item.setStaffId(actualPayload.getStaffId());
+                            item.setStaffName(actualPayload.getStaffName());
+                            item.setStaffQualifaction(actualPayload.getStaffQualifaction());
+                            item.setStaffDepartment(actualPayload.getStaffDepartment());
+                            item.setStaffAddress(actualPayload.getStaffAddress());
+                            item.setStaffSalary(actualPayload.getStaffSalary());
+                        }
+                )
+                .collect(Collectors.toList());
+
+
+        return updatedStaff;
+    }
+
+    public String modifyField(long id,StaffModel actualPayload){
+        for(StaffModel data : staff){
+            if(data.getStaffId() == id ){
+                data.setStaffName(actualPayload.getStaffName());
+                return "Record Modified Success";
+            }
+        }
+        return "ID Not found";
+    }
+
+    public String removeStaff(long id){
+
+        boolean isDeleted = staff
+                .removeIf(item -> item != null && Objects.equals(item.getStaffId(),id));
+
+        System.out.println(isDeleted);
+
+        return isDeleted ? "Staff Details Removed successfully" : "ID Not Found";
+    }
+
+
+
+
 }
